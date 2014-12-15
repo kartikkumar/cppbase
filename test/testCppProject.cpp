4f751f47b0af609b8dbc53ab84256571b2c330f1
Kartik Kumar <me@kartikkumar.com>
2014-12-15 13:54:54 +0100

    Update git ignore to not ignore test/ and add test/ to repository.

---
+++ test/testCppProject.cpp
@@ -0,0 +1,9 @@
+/*    
+ * Copyright (c) <year> <author> (<email>)
+ * Distributed under the MIT License.
+ * See accompanying file LICENSE.md or copy at http://opensource.org/licenses/MIT
+ */
+
+#define CATCH_CONFIG_MAIN
+
+#include <catch.hpp>
---
+++ test/testFactorial.cpp
@@ -0,0 +1,26 @@
+/*    
+ * Copyright (c) <year> <author> (<email>)
+ * Distributed under the MIT License.
+ * See accompanying file LICENSE.md or copy at http://opensource.org/licenses/MIT
+ */
+ 
+#include <catch.hpp>
+
+#include "CppProject/factorial.hpp"
+
+namespace cpp_project
+{
+namespace tests
+{
+
+TEST_CASE( "Test factorial function", "[factorial]" )
+{
+	REQUIRE_THROWS( computeFactorial( -5 ) );
+    REQUIRE( computeFactorial( 0 ) == 1 );
+    REQUIRE( computeFactorial( 1 ) == 1 );
+    REQUIRE( computeFactorial( 2 ) == 2 );
+    REQUIRE( computeFactorial( 5 ) == 120 );
+}
+
+} // namespace tests
+} // namespace cpp_project
