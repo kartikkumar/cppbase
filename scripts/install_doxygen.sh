17ca043920066d1d50d81f7b012218d139edbb33
Kartik Kumar <me@kartikkumar.com>
2016-01-12 20:28:41 +0100

    Change script to install updated Doxygen version for Travis setup.

---
+++ scripts/install_doxygen.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Copyright (c) 2014-2015 Kartik Kumar (me@kartikkumar.com)
+# Distributed under the MIT License.
+# See accompanying file LICENSE.md or copy at http://opensource.org/licenses/MIT
+
+set -ev
+
+# Fetch and build updated version of Doxygen from source.
+wget http://ftp.stack.nl/pub/users/dimitri/doxygen-1.8.11.src.tar.gz
+tar -xzvf doxygen-1.8.11.linux.bin.tar.gz
+cd doxygen-1.8.11 && ./configure --prefix=$HOME/doxygen && make && make install
