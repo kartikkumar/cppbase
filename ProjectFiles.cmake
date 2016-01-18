30d4a065079932fc16733665d8a5fd9264e2dd11
Kartik Kumar <me@kartikkumar.com>
2016-01-18 17:26:00 +0100

    Add missing file header in ProjectFiles.cmake.
    Correct include header guard in factorial.hpp.
    Fix formatting in testFactorial.cpp.

--- ProjectFiles.cmake
+++ ProjectFiles.cmake
@@ -1,3 +1,7 @@
+# Copyright (c) <year> <author> (<email>)
+# Distributed under the MIT License.
+# See accompanying file LICENSE.md or copy at http://opensource.org/licenses/MIT
+
 # Set project source files.
 set(SRC
   "${SRC_PATH}/factorial.cpp"
--- include/CppProject/factorial.hpp
+++ include/CppProject/factorial.hpp
@@ -4,8 +4,8 @@
  * See accompanying file LICENSE.md or copy at http://opensource.org/licenses/MIT
  */
 
-#ifndef CPP_PROJECT_HPP
-#define CPP_PROJECT_HPP
+#ifndef CPP_PROJECT_FACTORIAL_HPP
+#define CPP_PROJECT_FACTORIAL_HPP
 
 namespace cpp_project
 {
@@ -13,6 +13,7 @@ namespace cpp_project
 //! Compute factorial.
 /*!
  * Computes factorial of a given integer.
+ *
  * @param  integerNumber Integer number to compute factorial of
  * @return               Value of factorial of integer
  */
@@ -20,4 +21,4 @@ const int computeFactorial( const int integerNumber );
 
 } // namespace cpp_project
 
-#endif // CPP_PROJECT_HPP
+#endif // CPP_PROJECT_FACTORIAL_HPP
