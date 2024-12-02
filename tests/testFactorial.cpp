b004bb658c756f5524ff38bf8edb87055f14159a
Kartik Kumar <kartik@satsearch.com>
2024-12-02 12:01:24 +0100

    Update Copyright statement.

--- tests/testFactorial.cpp
+++ tests/testFactorial.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014-2022 Kartik Kumar (me@kartikkumar.com)
+ * Copyright (c) 2014-2024 Kartik Kumar (me@kartikkumar.com)
  * Distributed under the MIT License.
  * See accompanying file LICENSE or copy at http://opensource.org/licenses/MIT
  */
