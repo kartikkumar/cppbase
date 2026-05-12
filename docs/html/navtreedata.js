dce37e98206a4436379ce18a3d619c920d03d9b2
Kartik Kumar <kartik@satsearch.com>
2026-05-12 12:01:21 +0200

    Update Doxygen docs.

--- docs/html/navtreedata.js
+++ docs/html/navtreedata.js
@@ -43,6 +43,6 @@ var NAVTREEINDEX =
 "cppbaseAll_8hpp.html"
 ];
 
-var SYNCONMSG = 'click to disable panel synchronization';
-var SYNCOFFMSG = 'click to enable panel synchronization';
-var LISTOFALLMEMBERS = 'List of all members';
+const SYNCONMSG = 'click to disable panel synchronization';
+const SYNCOFFMSG = 'click to enable panel synchronization';
+const LISTOFALLMEMBERS = 'List of all members';
